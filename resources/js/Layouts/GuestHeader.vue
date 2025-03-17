acdd0330b0b1fdc7da4ba505bf78e715540300fc
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-03-17 13:11:38 +0800

    Update GuestHeader.vue

--- resources/js/Layouts/GuestHeader.vue
+++ resources/js/Layouts/GuestHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class="flex sticky items-center justify-start md:justify-between md:items-center bg-emerald-900 px-4 py-1 z-200">
+    <header class="flex sticky justify-start md:justify-between md:items-center bg-emerald-900 px-4 py-1 z-200">
         <!-- Mobile menu toggle button -->
-        <button @click="toggleMobileMenu" class="text-gray-500 focus:outline-none lg:hidden mr-2">
+        <button @click="toggleMobileMenu" class="text-gray-500 focus:outline-none md:hidden mr-2">
             <svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
@@ -18,36 +18,37 @@
         </div>
 
         <!-- Desktop Navigation Links -->
-        <nav class="hidden md:flex text-sm px-2 py-0 justify-center min-h-full items-center space-x-1 rounded text-white">
+        <nav class="hidden md:flex text-sm p-1 justify-center items-center space-x-1 rounded text-white">
             <NavLink 
                 :href="route('home')" 
                 :active="route().current('home')" 
-                class="nav-link hover:bg-emerald-600 px-1 py-2 rounded-lg transition-all duration-200"
+                class="nav-link hover:bg-emerald-600 px-4 py-2 rounded-lg transition-all duration-200"
             >
                 Home
             </NavLink>
             <NavLink 
                 :href="route('mapping')" 
                 :active="route().current('mapping')" 
-                class="nav-link hover:bg-emerald-600 px-1 py-2 rounded-lg transition-all duration-200"
+                class="nav-link hover:bg-emerald-600 px-4 py-2 rounded-lg transition-all duration-200"
             >
                 Mapping
             </NavLink>
             <NavLink 
                 :href="route('complaint')" 
                 :active="route().current('complaint')" 
-                class="nav-link hover:bg-emerald-600 px-1 py-2 rounded-lg transition-all duration-200"
+                class="nav-link hover:bg-emerald-600 px-4 py-2 rounded-lg transition-all duration-200"
             >
-                Issues And Complains
+                Issues And Complaints
             </NavLink>
             <NavLink 
                 :href="route('login')" 
                 :active="route().current('login')" 
-                class="nav-link hover:bg-emerald-600 px-1 bg-red-500 py-2 rounded-lg transition-all duration-200"
+                class="nav-link hover:bg-emerald-600 px-4 bg-red-500 py-2 rounded-lg transition-all duration-200"
             >
                 Login
             </NavLink>
         </nav>
+
     </header>
     <!-- Mobile Navigation Links (Visible when mobile menu is active) -->
     <nav :class="showingMobileMenu ? 'block' : 'hidden'" class="bg-emerald-800 text-white shadow-lg space-y-4 p-4 z-50">
