aa518b6e94915770aaabcdfb0458f5a2e5da1a94
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-03-19 02:09:24 +0800

    Update local_government_unit.sql

--- database/updatedb/local_government_unit.sql
+++ database/updatedb/local_government_unit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2025 at 06:56 PM
+-- Generation Time: Mar 18, 2025 at 07:09 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
